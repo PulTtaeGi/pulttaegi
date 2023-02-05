91db66ed7a2c9297be4ebda32500e83ee04e7920
ulimsss <z_o__o@naver.com>
2023-02-05 22:12:33 +0900

    초기 환경 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import { useState } from "react";
+import "./tailwind.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <div className="App">Pulttaegi</div>;
+  return (
+    <div className="App text-3xl underline ">
+      <button className="btn btn-primary">Pulttaegi</button>
+    </div>
+  );
 }
 
 export default App;
