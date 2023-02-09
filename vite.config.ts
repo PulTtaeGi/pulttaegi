7d67d3b718866d4e240824bd50c31ff3d63ddb35
ulimsss <70708829+ulimsss@users.noreply.github.com>
2023-02-10 02:04:14 +0900

    Revert "Revert "Jinwon""

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import VitePluginHtmlEnv from 'vite-plugin-html-env'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    VitePluginHtmlEnv(),
+    VitePluginHtmlEnv({compiler: true})],
 })
