6e9e1e805a92f939c874ad0c84d65008edd940e9
ulimsss <z_o__o@naver.com>
2023-02-19 23:08:27 +0900

    초기 세팅 커밋

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import VitePluginHtmlEnv from "vite-plugin-html-env";
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    VitePluginHtmlEnv(),
-    VitePluginHtmlEnv({ compiler: true }),
-  ],
-});
-=======
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import VitePluginHtmlEnv from 'vite-plugin-html-env'
@@ -22,4 +8,3 @@ export default defineConfig({
     VitePluginHtmlEnv(),
     VitePluginHtmlEnv({compiler: true})],
 })
->>>>>>> a65aac8b9ee6ccdc717ba57fa5aa27edb0e6c7ab
