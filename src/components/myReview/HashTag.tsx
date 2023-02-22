939b19f7e25977ce2aaabb1bbe6281648e6893c3
rangggu <aliyah521@naver.com>
2023-02-22 23:54:29 +0900

    리뷰 작성 기능 - 유저 전역변수 추가

--- src/components/myReview/HashTag.tsx
+++ src/components/myReview/HashTag.tsx
@@ -1,31 +1,23 @@
 import React from "react";
 import "../../tailwind.css";
 import styles from "../../styles/ActiveClass.module.css";
-import { targetReviewsType } from "../../pages/Detail";
 
 interface HashTagBarProps {
-  reviewList: Array<targetReviewsType>
+  list: Array<string>
 }
 
-export default function HashTagBar({ reviewList }: HashTagBarProps) {
-  const hashtagList : Array<string> = []
-  reviewList.map((review) => {
-    review.hashtag.map((item) => {
-      hashtagList.push(item)
-    })
-  }, [reviewList])
-
+export default function HashTag({ list }: HashTagBarProps) {
   return (
     <ul
       className={`${styles.hashTagBar} flex gap-3 w-[350px] mt-3 overflow-auto whitespace-nowrap text-lg`}
     >
-      {hashtagList.map((item: string, index: number) => {
+      {list.map((item: string, index: number) => {
         return (
           <li
             key={index}
             className="text-base font-medium mb-2 p-[3px] px-3 bg-green-3 text-white rounded-xl shadow-md shadow-gray-300"
           >
-            #{item}
+            {item}
           </li>
         );
       })}
