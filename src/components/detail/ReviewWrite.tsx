924d39525ae29600f2c51c1ee225ebf0069c93bf
rangggu <aliyah521@naver.com>
2023-02-25 19:36:32 +0900

    리뷰 작성 기능 - 로그인 여부에 따른 작성 권한 수정

--- src/components/detail/ReviewWrite.tsx
+++ src/components/detail/ReviewWrite.tsx
@@ -1,17 +1,33 @@
 import PrimaryButton from "../PrimaryButton";
+import { useAppSelector } from "../../store/hooks/configureStore.hook";
+import { useState, useEffect } from "react"
 
 interface ReviewWriteProps {
   title: string;
 }
 
 const ReviewWrite = ({title} : ReviewWriteProps) => {
+  const user = useAppSelector((state) => state.signup)
+  const [isLogin, setIsLogin] = useState(false)
+  const [isActive, setIsActive] = useState("hidden")
+
+  useEffect(() => {
+    user.signupUserInfo.id ? setIsLogin(true) : null
+  }, [])
+
+  function handleWrite() {
+    if(isLogin === false) {
+      setIsActive("block")
+    } return
+  }
+
   return (
     <div className="relative flex w-full items-center justify-center pb-12">
       <div className="flex flex-col gap-2">
-        <PrimaryButton color="green-3" link={`/review/${title}`}>
-          리뷰 작성하기
+        <PrimaryButton color="green-3" link={isLogin ? `/review/${title}` : "no"}>
+          <div style={{width: "300px"}} onClick={handleWrite}>리뷰 작성하기</div>
         </PrimaryButton>
-        <span className="w-full tracking-tight text-lg">
+        <span className={`${isActive} w-full tracking-tight text-lg`}>
           리뷰 작성은 로그인 후 이용 가능합니다.
         </span>
       </div>
