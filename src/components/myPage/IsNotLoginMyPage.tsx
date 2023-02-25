31f538428b22daa195c10adb924dcf2c3875188b
JinWon <dlfjs9514@nate.com>
2023-02-25 21:17:23 +0900

    뒤로가기버튼 추가

--- src/components/myPage/IsNotLoginMyPage.tsx
+++ src/components/myPage/IsNotLoginMyPage.tsx
@@ -1,8 +1,9 @@
 import { Link } from "react-router-dom";
-
+import BackArrow from "../common/BackArrow";
 const IsNotLoginMyPage = (): JSX.Element => {
   return (
     <div className="flex flex-col items-center mt-80">
+      <BackArrow />
       <div className="">로그인 후 이용해주세요.</div>
       <button className="btn w-36 mt-12 bg-myGreen border-myGreen  hover:bg-myDarkGreen hover:border-myGreen">
         <Link to="/login">로그인하러 가기</Link>
