94f9b6111574f81e287ca1d9c5365459033cd813
seongjun3880 <115451727+seongjun3880@users.noreply.github.com>
2023-03-04 15:23:15 +0900

    favorite button 로컬아이디저장

--- src/pages/Total.tsx
+++ src/pages/Total.tsx
@@ -2,17 +2,33 @@ import TabBar from "../layouts/tabBar";
 import { collection, getDocs, deleteDoc, doc } from "firebase/firestore";
 import { useEffect, useState } from "react";
 import { firestore } from "../api/firebase";
-import reviewimage from "../assets/icons/1-6.png";
-import BackArrow from "../components/common/BackArrow";
 import BreadCrumb from "../components/common/BreadCrumb";
-import FavoriteButton from "../components/common/FavoriteButton";
+import { getFirestore } from "firebase/firestore";
 
-function Second() {
+
+
+
+async function Second() {
   const favoritesCollectionRef = collection(firestore, "favorites");
+  
+  
   const [favorites, setFavorites] = useState<any[]>([]);
   const myfavorites : any[] = []
+
+    // const storedUserId = 
+    // const currentId = localStorage.getItem("id");
+   
+    
+    // if(storedUserId === currentId)
+    
+     
+  
   useEffect(() => {
+
+   
+
     async function fetchData() {
+      
       const data = await getDocs(favoritesCollectionRef);
       setFavorites(
         data.docs.map((doc) => {
@@ -30,32 +46,36 @@ function Second() {
 
   return (
     <div>
-      <BackArrow />
       <br></br>
       <br></br>
-      <br></br>
-      <BreadCrumb count={myfavorites.length}>  즐겨찾기</BreadCrumb>
+      <BreadCrumb count={myfavorites.length}>즐겨찾기</BreadCrumb>
       {favorites.map((favorite) => {
         return <Component docData={favorite} key={favorite.id} />;
       })}
       <TabBar />
     </div>
+
+      
   );
 }
 
 export default Second;
 
 const Component = ({ docData }: { docData: any }) => {
-  console.log (docData)
+
+  
+   console.log (docData)
+
+  
   return (
     <div className="text-black bg-white flex flex-col h-screen gap-7 justify-center items-center">
       <div className="flex flex-col gap-4">
         {docData.data.title}
         <div className="flex justify-between w-80 items-center   ">
           <div className="flex items-center   gap-4">
-            <img className="rounded-full" src={reviewimage} width="80" />
+            <img className="rounded-full" src={docData.data.img} width="80" />
             <div>
-              맛집<div>대표 메뉴: {}</div>
+              맛집<div>대표 메뉴: {docData.data.menu[0].menuName}</div>
             </div>
           </div>
           <button
