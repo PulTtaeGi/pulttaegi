1435fad6ef7363d5d1e45a6159a2b93e57ae3f05
rangggu <aliyah521@naver.com>
2023-03-06 01:50:32 +0900

    이미지 업로드 기능 구현 - 파이어베이스 스토리지

--- src/components/review/ImgUpload.tsx
+++ src/components/review/ImgUpload.tsx
@@ -1,70 +1,95 @@
 import { useEffect, useMemo, useRef, useState } from "react";
-
-type UploadImage = {
-  file: File;
-  thumbnail: string;
-  type: string;
-};
+import {
+  getStorage,
+  listAll,
+  ref,
+  uploadBytes,
+  getDownloadURL,
+} from "firebase/storage";
+import app from "../../api/firebase";
 
 interface ImgUploadProps {
   getUrl: (currentUrl: string) => void;
+  title: string | undefined;
+  prevUrl: string;
 }
 
-const ImgUpload = ({ getUrl }: ImgUploadProps): JSX.Element => {
+const ImgUpload = ({ getUrl, title, prevUrl }: ImgUploadProps): JSX.Element => {
   const fileInputRef = useRef<HTMLInputElement>(null);
-  const [imageFile, setImageFile] = useState<UploadImage | null>(null);
+  const [imageFile, setImageFile] = useState<any>();
+  const [preloadUrl, setPreloadUrl] = useState<string>();
+  const [existedImg, setExistedImg] = useState<any>();
+  const storage = getStorage(app);
+  const currentId = localStorage.getItem("id");
 
   const upLoadImg = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const fileList = e.target.files;
-    const length = fileList?.length;
-    if (fileList && fileList[0]) {
-      const url = URL.createObjectURL(fileList[0]);
-
-      setImageFile({
-        file: fileList[0],
-        thumbnail: url,
-        type: fileList[0].type.slice(0, 5),
+    if (e.target.files === null || !e.target.files) return;
+    setImageFile(e.target.files[0]);
+    uploadBytes(
+      ref(storage, `images/${title}/${currentId}/${e.target.files[0].name}`),
+      e.target.files[0]
+    ).then((snapshot) => {
+      getDownloadURL(snapshot.ref).then((url) => {
+        setPreloadUrl(url);
       });
-    }
+    });
   };
 
   const handleClickFileInput = () => {
     fileInputRef.current?.click();
   };
 
-  useEffect(() => {
-    if (imageFile?.thumbnail) {
-      getUrl(imageFile.thumbnail);
-    } else {
-      return;
-    }
-  }, [imageFile]);
-
   const showImage = useMemo(() => {
-    if (!imageFile && imageFile === null) {
+    if (!imageFile && imageFile === undefined) {
       return <div className="mt-4"></div>;
+    } else {
+      return (
+        <img
+          src={preloadUrl}
+          alt={imageFile.type}
+          className="w-80 h-56 mx-auto my-2"
+          onClick={handleClickFileInput}
+        />
+      );
     }
-    return (
-      <img
-        src={imageFile.thumbnail}
-        alt={imageFile.type}
-        className="w-80 h-56 mx-auto my-2"
-        onClick={handleClickFileInput}
-      />
-    );
-  }, [imageFile]);
+  }, [preloadUrl]);
+
+  useEffect(() => {
+    if (preloadUrl) {
+      getUrl(preloadUrl);
+    } else return;
+  }, [preloadUrl]);
+
+  useEffect(() => {
+    const storage = getStorage(app, "gs://pulttaegi-37599.appspot.com");
+    const currentImgRef = ref(storage, `images/${title}/${currentId}`);
+
+    listAll(currentImgRef).then((response) => {
+      getDownloadURL(response.items[0]).then((url) => {
+        setExistedImg(url);
+      });
+    });
+  }, []);
 
   return (
     <div className="flex flex-col">
       {showImage}
-      {/* <form method="post" encType="multipart/form-data"> */}
-      <div className="mt-4">
-        <label
-          htmlFor="chooseFile"
-          className="p-3 px-4 text-white text-base font-bold rounded-xl text-center whitespace-nowrap w-[180px] bg-green-3 border-green-3 hover:bg-green-4 hover:border-green-4"
-        >
-          사진 첨부하기
-        </label>
+      <div className="">
+        {existedImg && (
+          <img
+            src={existedImg}
+            className="w-80 h-56 mx-auto my-2"
+            onClick={handleClickFileInput}
+          />
+        )}
+        {!existedImg && (
+          <label
+            htmlFor="chooseFile"
+            className="p-3 px-4 text-white text-base font-bold rounded-xl text-center whitespace-nowrap w-[180px] bg-green-3 border-green-3 hover:bg-green-4 hover:border-green-4"
+          >
+            사진 첨부하기
+          </label>
+        )}
       </div>
       <input
         type="file"
@@ -74,7 +99,6 @@ const ImgUpload = ({ getUrl }: ImgUploadProps): JSX.Element => {
         onChange={upLoadImg}
         className="invisible absolute"
       />
-      {/* </form> */}
     </div>
   );
 };
